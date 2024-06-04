6e42363dc77492b4d8d3631d964f59a8bec47ee0
Vincent Doh <99025815+vin-doh@users.noreply.github.com>
2024-06-04 07:43:45 +0000

    blogvin modified

--- company/deployment.py
+++ company/deployment.py
@@ -21,6 +21,13 @@ MIDDLEWARE = [
 ] 
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/')
+]
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
--- company/settings.py
+++ company/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
